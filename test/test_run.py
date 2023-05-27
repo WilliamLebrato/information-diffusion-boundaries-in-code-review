9afb41c4972a0552411826d8f6d2a77bb7234262
Kalbz <38554807+Kalbz@users.noreply.github.com>
2023-05-27 14:29:02 +0200

    Tried testing arguments, someone perhaps knows

--- test/test_run.py
+++ test/test_run.py
@@ -1,7 +1,10 @@
 import timeit
 import unittest
 from simulation.model import CommunicationNetwork
-from simulation.run import run_simulation
+from simulation.run import run_simulation, argparse
+from unittest.mock import patch
+
+
 
 class TestPerformance(unittest.TestCase):
     @unittest.skip("Performance test is skipped due to the long execution time")
@@ -12,3 +15,14 @@ class TestPerformance(unittest.TestCase):
         print(f"Execution time: {end_time - start_time} seconds")
 
     test_performance()
+
+# class TestArguments(unittest.TestCase):
+#     @patch('argparse.ArgumentParser.parse_args',
+#            return_value=argparse.Namespace(param1="value1", param2="value2"))
+#     def test_argument_parsing(self):
+#         args = run_simulation()
+#         self.assertEqual(args.param1, "value1")
+#         self.assertEqual(args.param2, "value2")
+
+# if __name__ == "__main__":
+#     unittest.main()
