184e31ba513a6a207b86a36bc8aa742bdd0a6792
Todde01 <t.qvarlander@gmail.com>
2023-06-02 15:03:22 +0200

    :P

--- test/test_minimal_paths.py
+++ test/test_minimal_paths.py
@@ -4,8 +4,6 @@ import simulation.model
 from simulation.model import CommunicationNetwork
 from simulation.minimal_paths import single_source_dijkstra_vertices, single_source_dijkstra_hyperedges, single_source_bellman_ford_hypergraph, DistanceType
 
-
-
 class MinimalPath(unittest.TestCase):
     cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
 
@@ -33,7 +31,6 @@ class MinimalPath(unittest.TestCase):
         result_2 = single_source_dijkstra_hyperedges(cn_very_large, 'v1', DistanceType.SHORTEST, min_timing=0)
         self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
 
-
     def test_bellman_ford_shortest_path(self):
         result_bellman_ford = single_source_bellman_ford_hypergraph(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
         self.assertEqual(result_bellman_ford, {'v2': 1, 'v3': 2, 'v4': 3}, 'Bellman-Ford failed to find the shortest path')
@@ -42,8 +39,6 @@ class MinimalPath(unittest.TestCase):
         result_bellman_ford = single_source_bellman_ford_hypergraph(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
         result_dijkstra = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
         self.assertEqual(result_bellman_ford, result_dijkstra, 'Bellman-Ford and Dijkstra implementations are not equivalent')
-
-
 
 class MinimalPathExceptionHandling(unittest.TestCase):
         def test_minimal_path_unknown_vertice(self):
@@ -64,17 +59,16 @@ class TestBoundaryCases(unittest.TestCase):
         with self.assertRaises(simulation.model.EntityNotFound):
             single_source_dijkstra_vertices(TestBoundaryCases.cn_empty, 'v1', DistanceType.SHORTEST, min_timing=0)
             single_source_dijkstra_hyperedges(TestBoundaryCases.cn_empty, 'v1', DistanceType.SHORTEST, min_timing=0)
-
     
     @unittest.skip("fails")
-    def test_single_vertex(self): # (FAILS)
+    def test_single_vertex(self):
         self.assertEqual(single_source_dijkstra_vertices(TestBoundaryCases.cn_single_vertex, 'v1', DistanceType.SHORTEST, min_timing=0), {'v1': 0}, 'Provided a graph of only one vertex the expected result is a dictionary with one key and the value 0.')
         self.assertEqual(single_source_dijkstra_hyperedges(TestBoundaryCases.cn_single_vertex, 'v1', DistanceType.SHORTEST, min_timing=0), {'v1': 0}, 'Provided a graph of only one vertex the expected result is a dictionary with one key and the value 0.')
 
 class TestNegativeWeights(unittest.TestCase):
     cn_negative_weights = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': -1, 'h2': -2, 'h3': -3})
 
-        # Dijkstras algorithm should not handle negative weights, therefore an exeption is expected. (FAILS)
+        # Dijkstras algorithm should not handle negative weights, therefore an exeption is expected.
     @unittest.skip("fails")
     def test_negative_weights(self):
         with self.assertRaises(Exception):
